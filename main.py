1e44df7cc70dbb99c9ab14eb26266bf71d05722f
octorock <79596758+octorock@users.noreply.github.com>
2021-12-30 16:09:38 +0100

    Followed tutorial

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-
-from flask import Flask, jsonify, redirect, url_for
+from flask import Flask, jsonify, redirect, render_template, url_for
 from flask_dance.contrib.github import github
+from flask_login import logout_user, login_required
 
+from app.models import db, login_manager
 from app.oauth import github_blueprint
 
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./users.db"
 app.register_blueprint(github_blueprint, url_prefix="/login")
+
+db.init_app(app)
+login_manager.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+
 
 
 @app.route("/ping")
@@ -31,6 +47,12 @@ def login():
 
     return f"You are @{res.json()['login']} on GitHub"
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("homepage"))
+
 @app.route("/create_pr")
 def create_pr():
     if not github.authorized:
@@ -53,3 +75,6 @@ def create_pr():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
